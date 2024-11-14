dafab9ef27bea07b8242712abdc8c0e6ec648501
ReformedTheo <me@theo.tec.br>
2024-11-13 22:27:07 -0300

    Initial Commit

---
+++ iCalculator/iCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalculatorApp.swift
+//  iCalculator
+//
+//  Created by Theo Lopes on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct iCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
